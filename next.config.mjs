537cff05fc90fe8afabda815e9b280e126fd4ef8
Solomon Ibe <sibe8725@gmail.com>
2026-09-09 09:17:14 +0100

    Configure next/image to allow Sanity CDN images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import { withSentryConfig } from "@sentry/nextjs";
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
